b743ba982785f911d99b05595027b79cfc11310e
Austin Asbury <acasbury25@aol.com>
2019-08-06 21:05:29 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+def position_taken? (board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
+  else
+    true
+  end
+end
+
+def valid_move? (board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    true
+  else
+    false
+  end
+end
